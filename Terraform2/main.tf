92adc56679cee0120a8bb343fd69a5bf115f7d99
Taha-coder <56972033+Taha-coder@users.noreply.github.com>
2024-08-17 21:57:18 +0500

    Update main.tf

--- Terraform2/main.tf
+++ Terraform2/main.tf
@@ -89,8 +89,8 @@ resource "aws_security_group" "eks_sg" {
 }
 
 # Create IAM Role for EKS Cluster
-resource "aws_iam_role" "eks_admin_role" {
-  name = "eks-admin-role"
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -106,15 +106,43 @@ resource "aws_iam_role" "eks_admin_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_admin_policy" {
-  role     = aws_iam_role.eks_admin_role.name
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role     = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Create IAM Role for EKS Node Group
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_policy" {
+  role     = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_policy" {
+  role     = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 # Create EKS Cluster
 resource "aws_eks_cluster" "example" {
   name     = "example-cluster"
-  role_arn  = aws_iam_role.eks_admin_role.arn
+  role_arn  = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
     subnet_ids = [
@@ -149,7 +177,7 @@ resource "aws_launch_template" "eks_launch_template" {
 resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.example.name
   node_group_name = "example-node-group"
-  node_role_arn   = aws_iam_role.eks_admin_role.arn
+  node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = [
     aws_subnet.subnet_a.id,
     aws_subnet.subnet_b.id
