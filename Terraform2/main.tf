f2adfa7fc9ef6b20badb3e9fbde91c617639d4f5
Taha-coder <56972033+Taha-coder@users.noreply.github.com>
2024-08-17 21:21:53 +0500

    Create main.tf

---
+++ Terraform2/main.tf
@@ -0,0 +1,108 @@
+provider "aws" {
+  region = "us-east-1" # Change this to your desired region
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "eks_worker" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "image-id"
+    values = ["ami-04a81a99f5ec58529"]
+  }
+}
+
+resource "aws_iam_role" "eks_admin_role" {
+  name = "eks-admin-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_admin_policy" {
+  role     = aws_iam_role.eks_admin_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_eks_cluster" "example" {
+  name     = "example-cluster"
+  role_arn  = aws_iam_role.eks_admin_role.arn
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.default.ids
+  }
+}
+
+resource "aws_launch_template" "eks_launch_template" {
+  name_prefix   = "eks-launch-template-"
+  image_id       = "ami-04a81a99f5ec58529"
+  instance_type  = "t2.medium"
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups            = [aws_security_group.default.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "eks-node"
+    }
+  }
+}
+
+resource "aws_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.example.name
+  node_group_name = "example-node-group"
+  node_role_arn   = aws_iam_role.eks_admin_role.arn
+  subnet_ids      = data.aws_subnets.default.ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 1
+  }
+
+  launch_template {
+    id      = aws_launch_template.eks_launch_template.id
+    version = "$Latest"
+  }
+
+  depends_on = [
+    aws_eks_cluster.example
+  ]
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.example.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = aws_eks_cluster.example.certificate_authority[0].data
+}
