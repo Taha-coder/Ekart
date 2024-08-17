67ac16f3a00608dde1e9114cced139ebaa314225
Taha-coder <56972033+Taha-coder@users.noreply.github.com>
2024-08-17 21:39:11 +0500

    Update main.tf

--- Terraform2/main.tf
+++ Terraform2/main.tf
@@ -6,25 +6,24 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
 data "aws_availability_zones" "available" {}
 
-resource "aws_eks_cluster" "example" {
-  name     = "example-cluster"
-  role_arn  = aws_iam_role.eks_admin_role.arn
+resource "aws_subnet" "subnet_a" {
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+}
 
-  vpc_config {
-    subnet_ids = [
-      data.aws_subnets.default.ids[0],
-      data.aws_subnets.default.ids[1]
-    ]
-  }
+resource "aws_subnet" "subnet_b" {
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_iam_role" "eks_admin_role" {
@@ -49,6 +48,18 @@ resource "aws_iam_role_policy_attachment" "eks_admin_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
+resource "aws_eks_cluster" "example" {
+  name     = "example-cluster"
+  role_arn  = aws_iam_role.eks_admin_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.subnet_a.id,
+      aws_subnet.subnet_b.id
+    ]
+  }
+}
+
 resource "aws_launch_template" "eks_launch_template" {
   name_prefix   = "eks-launch-template-"
   image_id       = "ami-04a81a99f5ec58529"
@@ -68,17 +79,13 @@ resource "aws_launch_template" "eks_launch_template" {
   }
 }
 
-resource "aws_security_group" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
 resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.example.name
   node_group_name = "example-node-group"
   node_role_arn   = aws_iam_role.eks_admin_role.arn
   subnet_ids      = [
-    data.aws_subnets.default.ids[0],
-    data.aws_subnets.default.ids[1]
+    aws_subnet.subnet_a.id,
+    aws_subnet.subnet_b.id
   ]
 
   scaling_config {
